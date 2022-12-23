6e9a3cb787763d9da706dfafa6e506706ebef4df
Tim01Bro <arthaos14@gmail.com>
2022-12-24 00:56:06 +0300

    Role user domain

---
+++ src/main/java/com/circus/domain/RoleUser.java
@@ -0,0 +1,13 @@
+package com.circus.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RoleUser {
+    private Long id;
+    private String nameRole;
+}
