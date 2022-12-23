047868abe3b492e5bfd3481474a9b0525a46db60
Tim01Bro <arthaos14@gmail.com>
2022-12-24 01:05:13 +0300

    Circus news

---
+++ src/main/java/com/circus/domain/CircusNews.java
@@ -0,0 +1,19 @@
+package com.circus.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+@ToString
+public class CircusNews {
+    private Long id;
+    private String newsName;
+    private String newsText;
+    private LocalDateTime date_publication;
+    private Long idAuthor;
+    private TagNews tagNews;
+}
