89e36499be8d46bab1e5ee3582082a94129489fd
Tim01Bro <arthaos14@gmail.com>
2022-12-24 00:48:54 +0300

    Init commit

---
+++ src/main/java/com/circus/AppCircus.java
@@ -0,0 +1,11 @@
+package com.circus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCircus {
+    public static void main(String[] args) {
+        SpringApplication.run(AppCircus.class,args);
+    }
+}
