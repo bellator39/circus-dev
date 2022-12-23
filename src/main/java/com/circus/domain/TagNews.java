0148282a01745b4a7c82fc1677884d4e9634b70b
Tim01Bro <arthaos14@gmail.com>
2022-12-24 00:58:11 +0300

    Tag domain

---
+++ src/main/java/com/circus/domain/TagNews.java
@@ -0,0 +1,13 @@
+package com.circus.domain;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+@ToString
+public class TagNews {
+    private Long id;
+    private String tagName;
+}
