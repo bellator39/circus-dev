3bf5cef1df5f87b0a4934913c9bbbdd193f41636
Tim01Bro <arthaos14@gmail.com>
2022-12-26 04:20:11 +0300

    Circus show single

--- src/main/java/com/circus/controller/CircusShowController.java
+++ src/main/java/com/circus/controller/CircusShowController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,6 +18,13 @@ public class CircusShowController {
     public String circusShowlist(Model model){
         model.addAttribute("circusShow",circusShowService.findAllCircusShow());
         return "circusshowlist";
+    }
+
+    @GetMapping("/circusshow/{id}")
+    public String circusShowlist(@PathVariable("id")Long id,
+                                 Model model){
+        model.addAttribute("circusShowCurr",circusShowService.getCircusShowById(id));
+        return "circusshowsingle";
     }
 
 }
