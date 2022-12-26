0b5f9b029954aded5fa8bfa3da10dbe4c89c25f8
Tim01Bro <arthaos14@gmail.com>
2022-12-26 04:22:43 +0300

    Find news by name

--- src/main/java/com/circus/controller/CircusShowController.java
+++ src/main/java/com/circus/controller/CircusShowController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,6 +27,12 @@ public class CircusShowController {
                                  Model model){
         model.addAttribute("circusShowCurr",circusShowService.getCircusShowById(id));
         return "circusshowsingle";
+    }
+
+    @PostMapping("/circusshow/find")
+    public String circuShowFind(@RequestParam("name")String name,Model model){
+        model.addAttribute("circusShow",circusShowService.findAllByName(name));
+        return "circusshowlist";
     }
 
 }
