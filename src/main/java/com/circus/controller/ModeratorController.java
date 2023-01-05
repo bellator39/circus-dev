0469891166ae2d8be1623b3ad9903a38a7c1f184
Tim01Bro <arthaos14@gmail.com>
2023-01-05 19:13:09 +0300

    Редактирование новости

--- src/main/java/com/circus/controller/ModeratorController.java
+++ src/main/java/com/circus/controller/ModeratorController.java
@@ -1,5 +1,6 @@
 package com.circus.controller;
 
+import com.circus.domain.CircusNews;
 import com.circus.service.api.CircusNewsServiceApi;
 import com.circus.service.api.TagNewsServiceApi;
 import lombok.RequiredArgsConstructor;
@@ -7,9 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.function.Function;
 
@@ -33,6 +32,25 @@ public class ModeratorController {
     @GetMapping("/delete/{id}")
     public String deleteNewsById(@PathVariable("id")Long id){
         circusNewsService.deleteCircusNews(id);
+        return "redirect:/moderator/allnews";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editPage(@PathVariable("id")Long id,Model model){
+        model.addAttribute("editNews",circusNewsService.getCircusNewsById(id));
+        return "admin/editnews";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editNewsById(@PathVariable("id")Long id,
+                               @RequestParam("name")String name,
+                               @RequestParam("Text")String text,
+                               @RequestParam("url")String url){
+        CircusNews circusNews = circusNewsService.getCircusNewsById(id);
+        circusNews.setNewsName(name);
+        circusNews.setNewsText(text);
+        circusNews.setUrllogonews(url);
+        circusNewsService.updateCircusNews(circusNews);
         return "redirect:/moderator/allnews";
     }
 
