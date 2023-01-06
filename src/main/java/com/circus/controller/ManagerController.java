2301596217f1bd9d5bc37a7b535ae94b3d1abfa4
Tim01Bro <arthaos14@gmail.com>
2023-01-06 19:25:02 +0300

    All feedback page

---
+++ src/main/java/com/circus/controller/ManagerController.java
@@ -0,0 +1,26 @@
+package com.circus.controller;
+
+import com.circus.service.api.ContactServiceApi;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@PreAuthorize("hasAuthority('MANAGER')")
+@RequestMapping("/manager")
+public class ManagerController {
+
+    private final ContactServiceApi contactService;
+
+    @GetMapping("/allfeedback")
+    public String allFeedback(Model model){
+        model.addAttribute("contactList",contactService.findAllContact());
+        return "admin/allfeedback";
+    }
+}
