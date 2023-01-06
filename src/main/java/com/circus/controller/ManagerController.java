aff271c551d428b0176a88399af7285f287bc937
Tim01Bro <arthaos14@gmail.com>
2023-01-06 19:31:44 +0300

    Delete feedback

--- src/main/java/com/circus/controller/ManagerController.java
+++ src/main/java/com/circus/controller/ManagerController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +23,11 @@ public class ManagerController {
     public String allFeedback(Model model){
         model.addAttribute("contactList",contactService.findAllContact());
         return "admin/allfeedback";
+    }
+
+    @GetMapping("/feedback/delete/{id}")
+    public String deleteFeedback(@PathVariable("id")Long idfeedback){
+        contactService.deleteContact(idfeedback);
+        return "redirect:/manager/allfeedback";
     }
 }
